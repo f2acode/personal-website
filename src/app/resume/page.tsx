fd273209e704fb50ce29784f1eadb2aae22e49ba
Felipe Augusto <augustto.felipe@hotmail.com>
2024-04-13 22:12:49 -0300

    style: skills spacing

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -93,7 +93,7 @@ export default function ResumePage() {
                   {category.skills.map((skill, index) => (
                     <span
                       key={index}
-                      className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+                      className="inline-flex items-center rounded-md bg-gray-50 px-2 mr-2 mb-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
                     >
                       {skill}
                     </span>
