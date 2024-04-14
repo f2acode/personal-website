08bbe02c771d211fe0142e32d1c95048dd9e0d09
Felipe Augusto <augustto.felipe@hotmail.com>
2024-04-13 22:11:55 -0300

    fix: tagline ATS links recognition and style

--- src/components/tagline.tsx
+++ src/components/tagline.tsx
@@ -60,16 +60,16 @@ export default function TagLine({ resumeData }: TagLineProps) {
           priority={true}
         />
       </div>
-      <div className="flex flex-col text-wrap w-4/6 mx-3 pt-4">
+      <div className="flex flex-col text-wrap w-4/6 mx-3 mt-4">
         <h1 className="text-2xl font-bold leading-7 text-gray-900 uppercase">
           {resumeData.basics.name}
         </h1>
         <h2 className="text-[#2451ff] font-medium">
           {resumeData.basics.label}
         </h2>
-        <div className="min-w-0 flex-1">
+        <div className="min-w-0 flex-1 space-y-2">
           <div className="mt-1 sm:divide-x-2 sm:*:px-4 sm:first:*:pl-0 sm:last:*:pr-0 sm:flex sm:flex-row">
-            <div className="mt-2 flex items-center text-sm text-gray-500">
+            <div className="sm:w-1/2 mt-2 flex items-center text-sm text-gray-500">
               <MapPinIcon
                 className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-500"
                 aria-hidden="true"
@@ -78,7 +78,7 @@ export default function TagLine({ resumeData }: TagLineProps) {
               {resumeData.basics.location.countryCode}
             </div>
             <Link
-              className="mt-2 flex items-center text-sm text-gray-500"
+              className="sm:w-1/2 mt-2 flex items-center text-sm text-gray-500"
               href={`mailto:${resumeData.basics.email}`}
               target="_blank"
             >
@@ -91,7 +91,7 @@ export default function TagLine({ resumeData }: TagLineProps) {
           </div>
           <div className="mt-1 sm:divide-x-2 sm:*:px-4 sm:first:*:pl-0 sm:last:*:pr-0 sm:flex sm:flex-row">
             <Link
-              className="mt-2 flex items-center text-sm text-gray-500"
+              className="sm:w-1/2 mt-2 flex items-center text-sm text-gray-500"
               href={`https://wa.me/${resumeData.basics.phone}`}
               target="_blank"
             >
@@ -102,20 +102,22 @@ export default function TagLine({ resumeData }: TagLineProps) {
               {resumeData.basics.phone}
             </Link>
             <Link
-              className="mt-2 flex items-center text-sm text-gray-500"
+              className="sm:w-1/2 mt-2 flex items-center text-sm text-gray-500"
               href={resumeData.basics.url}
               target="_blank"
             >
               <GithubIcon className="mr-1.5 h-5 w-5 fill-[#333]" />
-              {resumeData.basics.url.split('/')[3]}
+              {resumeData.basics.url}
             </Link>
+          </div>
+          <div className="mt-1 sm:divide-x-2 sm:*:px-4 sm:first:*:pl-0 sm:last:*:pr-0 sm:flex sm:flex-row">
             <Link
               className="mt-2 flex items-center text-sm text-gray-500"
               href={resumeData.basics.profiles[0].url}
               target="_blank"
             >
               <LinkedinIcon className="mr-1.5 h-5 w-5 fill-[#0077b5]"></LinkedinIcon>
-              {resumeData.basics.profiles[0].url.split('/')[4]}
+              {resumeData.basics.profiles[0].url}
             </Link>
           </div>
         </div>
